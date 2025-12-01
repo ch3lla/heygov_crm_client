540af6ae191b4dfcb43d334be2d1b8efcb391dc9
Chella <chukwumaemmanuella03@gmail.com>
2025-12-01 20:47:19 +0100

    debug: fix deployment errors with pinia

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+import 'pinia'
+
+declare module 'pinia' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  export interface DefineStoreOptionsBase<S, Store> {
+    persist?: boolean | {
+      key?: string
+      storage?: Storage
+      paths?: string[]
+    }
+  }
+}
